970e4645bf5ae8758653ba83834fa49963240d78
OkunaWAMAMU <vincentwamamu1@gmail.com>
2023-06-19 07:51:58 -0700

    using  the function puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - (this is a program that prints a line with puts function
+return -program will return 0
+ **/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
