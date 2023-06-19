040601cdb19756fa564316fa59447efa350db5f0
OkunaWAMAMU <vincentwamamu1@gmail.com>
2023-06-19 07:56:54 -0700

    using  the function puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  *main - (this is a program that prints a line with puts function
-return -program will return 0
- **/
+ *return: 0 (success)
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
