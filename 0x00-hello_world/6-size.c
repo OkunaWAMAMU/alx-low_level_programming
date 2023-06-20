531a868a5c06f590d841492a294b81191e9811b5
OkunaWAMAMU <vincentwamamu1@gmail.com>
2023-06-19 23:35:47 -0700

    correctiong

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -20,6 +20,6 @@ sizeof(c));
 printf("Size of a long long int: %lu byte(s)\n", (unsigned long)
 sizeof(d));
 printf("Size of a float: %lu byte(s)\n", (unsigned long)
-sizeof(f))
+sizeof(f));
 return (0);
 }
