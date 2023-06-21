44ebaf2cdc3f4a5893344f9a975cc1c06bb2d85a
OkunaWAMAMU <vincentwamamu1@gmail.com>
2023-06-21 15:09:27 -0700

    correction

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
  *main -  a program that prints alphabets
- * 
+ *
  * return: 0 (success)
  */
 void print_alphabet(void)
 {
 char vin;
-for(vin ='a'; vin <= 'z'; vin++)
+for (vin = 'a'; vin <= 'z'; vin++)
 {
 _putchar(vin);
 _putchar('\n');
 }
-
 }
