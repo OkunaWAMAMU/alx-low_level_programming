bdd36f081c99345f332fc2888ef655ff95179f29
OkunaWAMAMU <vincentwamamu1@gmail.com>
2023-06-21 13:47:45 -0700

    a function that prints the alphabet, in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <main.h>
+/**
+ * a program that prints alphabets
+ * 
+ * return 0 (success)
+ */
+void print_alphabet(void);
+{
+char vin = 'a';
+if(vin >='a' && vin <= 'z')
+{
+	putchar("'a' ");
+}
+return ();
+}
