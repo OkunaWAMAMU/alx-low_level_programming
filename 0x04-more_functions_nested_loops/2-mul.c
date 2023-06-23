6b15c8ccf239c41415d440600843e6002b32cf2a
OkunaWAMAMU <vincentwamamu1@gmail.com>
2023-06-23 09:04:23 -0700

    Task two

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - function that multiplies two numbers
+ * @a: number to be multiplied
+ * @b: number to be multiplied
+ * @product: the variable that stores the product
+ * return : the value of the multiplied numbers
+ **/
+
+int product;
+
+
+int mul(int a, int b)
+{
+	return a * b;
+}
