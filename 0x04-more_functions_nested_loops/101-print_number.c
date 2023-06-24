716e0d13be5aefbedfc3d9ba31d8dfd120d93b1d
OkunaWAMAMU <vincentwamamu1@gmail.com>
2023-06-24 00:41:55 -0700

    Task twelve

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -3,33 +3,20 @@
 /**
  * print_number - prints an integer
  * @n: the integer to be printed
- *
- * Description: This function prints the given integer using _putchar
- *              without using long, arrays, pointers, or hard-coded values.
  */
 void print_number(int n)
 {
-	int power = 1;
-	int temp = n;
-
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
-		temp = n;
 	}
 
-	while (temp >= 10)
+	if (n >= 10)
 	{
-		power *= 10;
-		temp /= 10;
+		print_number(n / 10);
 	}
 
-	while (power > 0)
-	{
-		_putchar((n / power) + '0');
-		n %= power;
-		power /= 10;
-	}
+	_putchar((n % 10) + '0');
 }
 
