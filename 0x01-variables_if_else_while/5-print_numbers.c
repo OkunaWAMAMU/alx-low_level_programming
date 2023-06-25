afb47cdc92b0861edf571087b3ce11e7b9f5bb62
OkunaWAMAMU <vincentwamamu1@gmail.com>
2023-06-25 05:46:13 -0700

    task five

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point of code
+ * Return: return value is 0
+ **/
+
+int main(void)
+{
+	int index = 0;
+
+	while (index < 10)
+	{
+		printf("%d", index);
+		index++;
+	}
+	printf("\n");
+	return (0);
+}
