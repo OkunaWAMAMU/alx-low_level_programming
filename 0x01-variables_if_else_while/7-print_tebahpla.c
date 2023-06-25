9769d85db8278d7379e0efdb55c7c82c4d0db106
OkunaWAMAMU <vincentwamamu1@gmail.com>
2023-06-25 06:00:32 -0700

    Task seven

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: the return value is 0
+ **/
+
+int main(void)
+{
+	char k;
+
+	for (k = 'z'; k >= 'a'; k--)
+	{
+	putchar(k);
+	}
+	putchar('\n');
+	return (0);
+}
+
