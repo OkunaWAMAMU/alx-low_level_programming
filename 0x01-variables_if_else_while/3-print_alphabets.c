0b52244f7394c53e4acad8c17aa1c8e02617726e
OkunaWAMAMU <vincentwamamu1@gmail.com>
2023-06-25 05:26:03 -0700

    Task three

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: the return value is 0
+ **/
+
+int main(void)
+{
+	char k;
+
+	for (k = 'a'; k <= 'z'; k++)
+	{
+		putchar(k);
+	}
+	for (k = 'A'; k <= 'Z'; k++)
+	{
+		putchar(k);
+	}
+	putchar('\n');
+	return (0);
+}
+
