54298410ab89da70fc9f641d89197092c873761f
OkunaWAMAMU <vincentwamamu1@gmail.com>
2023-06-25 05:02:40 -0700

    Task one

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 	}
 	if (n == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, n);
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
 	if ((n % 10) < 6 && (n % 10) != 0)
 	{
