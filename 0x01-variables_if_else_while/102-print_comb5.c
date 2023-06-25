7aba6a4f375cca71af8badbd54743c5dcd11a0cc
OkunaWAMAMU <vincentwamamu1@gmail.com>
2023-06-25 09:41:25 -0700

    Task twelve

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: the return value is 0
+ **/
+
+int main(void)
+{
+	int index, m;
+
+	for (index = 0; index <= 99; index++)
+	{
+	for (m = 0; m < 100; m++)
+	{
+		if (index < m)
+		{
+		putchar('0' + index / 10);
+		putchar('0' + index % 10);
+		putchar(' ');
+		putchar('0' + m / 10);
+		putchar('0' + m % 10);
+		if (index != 99 || m != 99)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
