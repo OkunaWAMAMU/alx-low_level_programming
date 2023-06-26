b6d37cf3b894a74afc8646d8945106071f4e37dc
OkunaWAMAMU <vincentwamamu1@gmail.com>
2023-06-26 08:57:17 -0700

    Task zero

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that resets int value to 98
+ * @n: integer parameter
+ * Return: return value is n
+ **/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+projects on arrays and pointers
