f5560d4aa304c8bf4487e51a3e196c2d14d07727
OkunaWAMAMU <vincentwamamu1@gmail.com>
2023-06-28 01:31:58 -0700

    Task four

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,14 +4,11 @@
  * @c: type
  * Return: 0 (success)
  */
+
 int _isalpha(int c)
 {
-	if ((c >= 'a' &&  c >= 'z') ||  (c <= 'A' && c <= 'Z'))
-	{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
