cc1d6d7912997013bd86c84e974c8039646eee76
OkunaWAMAMU <vincentwamamu1@gmail.com>
2023-06-30 01:32:39 -0700

    Task nine

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,15 +8,19 @@
 
 void print_number(int n)
 {
+	unsigned int index;
+
+	index = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		index = -n;
 	}
 
-	if (n / 10 != 0)
-		print_number(n / 10);
+	if (index / 10 != 0)
+		print_number(index / 10);
 
-	_putchar((n % 10) + '0');
+	_putchar((index % 10) + '0');
 }
 
