66f5ce4c6791371c9d2e24b8401a780f94a41f26
OkunaWAMAMU <vincentwamamu1@gmail.com>
2023-07-03 06:02:04 -0700

    Task eight

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,13 +10,13 @@
 
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+	int index = 0;
 
-	while (s[i] >=  '\0')
+	while (s[index] >=  '\0')
 	{
-	if (s[i] == c)
-	return (&s[i]);
-	i++;
+	if (s[index] == c)
+	return (&s[index]);
+	index++;
 	}
 	return (NULL);
 }
