ab0cdb5a0732c20a934227358fb2d2c23a572167
OkunaWAMAMU <vincentwamamu1@gmail.com>
2023-09-27 12:50:54 -0700

    still update

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,15 +4,19 @@
  * _puts_recursion - the main code
  * @s: string
  *
- * Return: always 0.
+ * Return: Always 0.
  */
+
 void _puts_recursion(char *s)
 {
-	if (*s)
-	{
+if (*s)
+{
 _putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
-		_putchar('\n');
+_puts_recursion(s + 1);
 }
+else
+{
+_putchar('\n');
+}
+}
+
