4aec9f375ba384026349ba9304b12f0aec7ffe36
OkunaWAMAMU <vincentwamamu1@gmail.com>
2023-09-27 07:01:47 -0700

    factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * factorial - find and  returns the factorial of a given number.
+ * n - numbers whose factorial is returned
+ *
+ * return: Always 0.
+ */
+
+int factorial(int n)
+{
+    if(n<=1)
+        {
+           return 1;
+        }
+    else
+    {
+      return n * factorial(n-1);
+    }
+}
+int main()
+{
+        printf("%d\n", factorial(5));
+    return 0;
+}
