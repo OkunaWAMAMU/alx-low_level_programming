a1c0ff006e97827ab940d18ea5a9f98158f6e466
OkunaWAMAMU <vincentwamamu1@gmail.com>
2023-10-19 18:51:22 -0700

    weeeu

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -30,7 +30,9 @@ list_t *add_node(list_t **head, const char *str)
 	while (str[new_node->len] != '\0')
 	{
 		new_node->len++;
+	}
 		new_node->next = *head;
 		*head = new_node;
+		
 		return (new_node);
 	}
