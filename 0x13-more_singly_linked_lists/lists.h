0440233f2076b3b29c6da41d8a54ca204eef6e01
OkunaWAMAMU <vincentwamamu1@gmail.com>
2023-10-24 03:10:09 -0700

    0-main.h update

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,6 +2,7 @@
 #define LISTS_H
 
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -20,4 +21,6 @@ typedef struct listint_s
 size_t print_listint(const listint_t *h);
 /*Function prototype for Returns the number of elements in a listint_t list.*/
 size_t listint_len(const listint_t *h);
+/*Function prototype for  adding a new node at the beginning of a listint_t list.*/
+listint_t *add_nodeint(listint_t **head, const int n);
 #endif
