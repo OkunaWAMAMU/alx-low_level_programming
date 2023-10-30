6e29e41a059164dcf30f9d7cc30eca8a7a749691
OkunaWAMAMU <vincentwamamu1@gmail.com>
2023-10-30 11:48:20 -0700

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,23 +10,23 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-ssize_t bytes_written = write(fd, text_content, strlen(text_content));
+	int fd;
+	ssize_t bytes_written;
+
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-
-		if (fd == -1)
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
 	{
 		return (-1);
 	}
 
 	if (text_content != NULL)
 	{
-
+		bytes_written = write(fd, text_content, strlen(text_content));
 		close(fd);
-
 		if (bytes_written == -1 || (size_t)bytes_written != strlen(text_content))
 		{
 			return (-1);
